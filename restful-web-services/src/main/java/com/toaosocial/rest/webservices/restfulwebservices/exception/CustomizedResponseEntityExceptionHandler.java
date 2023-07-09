2c403613636b9f269a76a1fb397760a54b744bf4
Zülfikar Karakaya <zulfikarakaya@gmail.com>
2023-07-09 20:09:14 +0200

    add method argument validation

--- restful-web-services/src/main/java/com/toaosocial/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ restful-web-services/src/main/java/com/toaosocial/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -2,8 +2,11 @@ package com.toaosocial.rest.webservices.restfulwebservices.exception;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -24,6 +27,14 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
 		
 		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND); 	
+	}	
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<Object>(errorDetails, HttpStatus.BAD_REQUEST); 	
 	}	
 	
 	
